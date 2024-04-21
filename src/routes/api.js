132a622b329a2449b197a5c82e8e77e0bc8f196c
Chí Cường GIáp <giapchicuong@gmail.com>
2024-04-21 19:53:47 +0700

    change requireds

--- src/routes/api.js
+++ src/routes/api.js
@@ -1,4 +1,4 @@
-import express from "express";
+const express = require("express");
 const router = express.Router();
 import userController from "../controller/userController";
 import { checkUserJwt, checkUserPermission } from "../middleware/JWTAction";
